c718e9d3e43d3b0190963d905bdcd9e587d2fd3b
Srinu Duggempudi <53874711+Srinu520@users.noreply.github.com>
2026-08-09 17:08:08 +0530

    Use production domain in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://sriramcabs.vercel.app";
+const siteUrl = "https://sreekhushitravels.in";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = [
