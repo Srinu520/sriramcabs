984bb810f2b1c2a302e4407dbf74337f6bffdda2
Srinu Duggempudi <53874711+Srinu520@users.noreply.github.com>
2026-08-09 17:08:17 +0530

    Use production domain in robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://sriramcabs.vercel.app";
+const siteUrl = "https://sreekhushitravels.in";
 
 export default function robots(): MetadataRoute.Robots {
   return {
